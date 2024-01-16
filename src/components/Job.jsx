539fb70fdd505ac4faecd785f48807c00b9cd655
Jonathan Rodriguez <jonathan20.rodriguez04@gmail.com>
2024-01-16 16:35:36 -0600

    Job card responsive font fixed

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -9,11 +9,11 @@ export default function Job({ companyName, jobName, jobType, location, postedAt 
                 </div>
                 <div>
                     <p className='text-sm font-bold'>{companyName}</p>
-                    <h1 className='text-lg'>{jobName}</h1>
+                    <h1 className='text-base lg:text-lg'>{jobName}</h1>
                     { jobType && <span className='text-xs border border-solid rounded-md py-1 px-2 font-semibold'>{jobType}</span> }
                 </div>
             </section>
-            <div className='text-sm flex flex-col items-end'>
+            <div className='text-xs  lg:text-sm flex flex-col items-end'>
                 <p className='text-gray'><i className="bi bi-globe-americas"></i> {location}</p>
                 <p className='text-gray ml-4'><i className="bi bi-clock"> </i>{postedAt}</p>
             </div>
